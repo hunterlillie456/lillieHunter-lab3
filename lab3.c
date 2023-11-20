9cbdbbd7238daa1aee6c265b269eae732ffeff53
hunterlillie456 <hunterlillie456@gmail.com>
2023-11-19 22:14:47 -0500

    commit 2

--- lab3.c
+++ lab3.c
@@ -39,14 +39,34 @@ int is_board_valid(){
     pthread_t* tid;  /* the thread identifiers */
     pthread_attr_t attr;
     param_struct* parameter;
-    
 
-    /* tid = (pthread_t*)malloc(sizeof(int)*NUM_OF_THREADS);
-    parameter = (param_struct*)malloc(sizeof(param_struct)*NUM_OF_THREADS);
-
-    pthread_create(&(tid[0], &attr, , &(params[0])));
-    */
+    tid = (pthread_t*)malloc(sizeof(int)*NUM_OF_THREADS);
+    parameter =(param_struct*)malloc(sizeof(param_struct)*NUM_OF_THREADS); 
     
+        for (int i = 0; i < 9; i++){
+        for (int j = 0; j < 9; j++)
+        {
+            
+            // Create thread for 3x3
+            if (i%3 == 0 && j%3 == 0)
+            {
+                
+            }
+
+            // Create thread for cols
+            if (j == 0)
+            {
+                
+            }
+
+            // Create thread for rows 
+            if (i == 0)
+            {
+                
+            }
+        }
+    }
+
     return 0;
 }
 
